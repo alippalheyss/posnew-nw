af6bb5c1444716876799439eb6ea94de59139cdb
alippalheyss <alippalheyss@gmail.com>
2026-01-28 16:14:09 +0500

    Fix context hierarchy bug: move AppContextProvider inside AuthProvider to prevent initialization crash

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AuthProvider } from "@/context/AuthContext";
+import { AppProviderWithPriceDialog } from "./context/AppProviderWithPriceDialog";
 import ProtectedRoute from "@/components/ProtectedRoute";
 import Layout from "./components/Layout";
 import Login from "./pages/Login";
@@ -29,39 +30,41 @@ const App = () => (
       <Toaster />
       <Sonner />
       <AuthProvider>
-        <BrowserRouter>
-          <Routes>
-            {/* Public Route */}
-            <Route path="/login" element={<Login />} />
+        <AppProviderWithPriceDialog>
+          <BrowserRouter>
+            <Routes>
+              {/* Public Route */}
+              <Route path="/login" element={<Login />} />
 
-            {/* Protected Routes */}
-            <Route
-              path="/*"
-              element={
-                <ProtectedRoute>
-                  <Layout>
-                    <Routes>
-                      <Route path="/" element={<POS />} />
-                      <Route path="/products" element={<Products />} />
-                      <Route path="/stock" element={<Stock />} />
-                      <Route path="/customers" element={<Customers />} />
-                      <Route path="/vendors" element={<Vendors />} />
-                      <Route path="/credit-outstanding" element={<CreditOutstanding />} />
-                      <Route path="/admin" element={<Admin />} />
-                      <Route path="/daily-sales" element={<DailySales />} />
-                      <Route path="/credit-invoices" element={<CreditInvoices />} />
-                      <Route path="/sales-reports" element={<SalesReports />} />
-                      <Route path="/expiry-alerts" element={<ExpiryAlerts />} />
-                      <Route path="/gst-reports" element={<GSTReports />} />
-                      {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-                      <Route path="*" element={<NotFound />} />
-                    </Routes>
-                  </Layout>
-                </ProtectedRoute>
-              }
-            />
-          </Routes>
-        </BrowserRouter>
+              {/* Protected Routes */}
+              <Route
+                path="/*"
+                element={
+                  <ProtectedRoute>
+                    <Layout>
+                      <Routes>
+                        <Route path="/" element={<POS />} />
+                        <Route path="/products" element={<Products />} />
+                        <Route path="/stock" element={<Stock />} />
+                        <Route path="/customers" element={<Customers />} />
+                        <Route path="/vendors" element={<Vendors />} />
+                        <Route path="/credit-outstanding" element={<CreditOutstanding />} />
+                        <Route path="/admin" element={<Admin />} />
+                        <Route path="/daily-sales" element={<DailySales />} />
+                        <Route path="/credit-invoices" element={<CreditInvoices />} />
+                        <Route path="/sales-reports" element={<SalesReports />} />
+                        <Route path="/expiry-alerts" element={<ExpiryAlerts />} />
+                        <Route path="/gst-reports" element={<GSTReports />} />
+                        {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+                        <Route path="*" element={<NotFound />} />
+                      </Routes>
+                    </Layout>
+                  </ProtectedRoute>
+                }
+              />
+            </Routes>
+          </BrowserRouter>
+        </AppProviderWithPriceDialog>
       </AuthProvider>
     </TooltipProvider>
   </QueryClientProvider>
